340fadcd49b943aad31378f07088c4063603fae8
João Pablo <jp.pablolima@gmail.com>
2023-05-18 18:00:56 -0300

    configuração para criação vm e vpc

---
+++ ec2/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_version = "~> 1.4"
+
+  required_providers {
+    aws = {
+      version = "~> 4.0"
+      source  = "hashicorp/aws"
+    }
+  }
+
+  backend "s3" {
+    bucket = "my-bucket-remote-state"
+    key    = "aws-vm/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region              = var.region
+  shared_config_files = ["~/.aws/credentials"]
+  profile             = "terraform"
+
+
+}
+
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "my-bucket-remote-state"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+
